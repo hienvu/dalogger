f778b7ff202fc3f6e01c74adb3fed53d307eb158
Hien Vu <hien@localhost>
2025-11-01 10:04:34 -0700

    add a quick examples readme

--- examples/queue/main.mjs
+++ examples/queue/main.mjs
@@ -5,14 +5,14 @@ import logger, { DaLogger } from '@hvu/dalogger';
  **  Each task will use its supplied trace key.
  **  Each task will randomly fail to simulate error scenarios.
  */
-function main() {
+async function main() {
   const tasks = [];
   for (let i = 1; i <= 3; i++) {
     tasks.push({ id: i });
   }
 
   for (const task of tasks) {
-    DaLogger.run(async () => {
+    await DaLogger.run(async () => {
       await processTask(task);
       logger().info('Task processed', task);
     }, `trace-key-${task.id}`);
